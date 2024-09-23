26820c46e171a93bd381d3d915e8be8a8a917851
adaooctopuscode <adaooctopuscode@gmail.com>
2024-09-23 16:08:26 -0300

    Persistindo dados com preferences

--- lib/snippets.dart
+++ lib/snippets.dart
@@ -9,6 +9,7 @@ OutlineInputBorder border({Color borderColor = Colors.black}) {
     borderRadius: const BorderRadius.all(Radius.circular(20.0)),
   );
 }
+
 const spaceHeight = SizedBox(height: 10);
 
 const spaceWidth = SizedBox(height: 10);
@@ -21,29 +22,26 @@ Widget icon({required IconData icon, required VoidCallback onTap}) {
 }
 
 InputDecoration inputDecoration({required String label}) => InputDecoration(
-  contentPadding:
-  const EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
-  labelText: label,
-  floatingLabelBehavior: FloatingLabelBehavior.always,
-  labelStyle: const TextStyle(fontSize: 15, color: Colors.black),
-  focusedBorder: border(borderColor: Colors.black),
-  enabledBorder: border(borderColor: Colors.black),
-  disabledBorder: border(borderColor: Colors.grey),
-  errorBorder: border(borderColor: Colors.red),
-  focusedErrorBorder: border(borderColor: Colors.red),
-  counterText: '',
-);
+      contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 15.0),
+      labelText: label,
+      floatingLabelBehavior: FloatingLabelBehavior.always,
+      labelStyle: const TextStyle(fontSize: 15, color: Colors.black),
+      focusedBorder: border(borderColor: Colors.black),
+      enabledBorder: border(borderColor: Colors.black),
+      disabledBorder: border(borderColor: Colors.grey),
+      errorBorder: border(borderColor: Colors.red),
+      focusedErrorBorder: border(borderColor: Colors.red),
+      counterText: '',
+    );
 
 openRoute(BuildContext context, Widget page) {
   Navigator.push(
     context,
-    MaterialPageRoute(
-        builder: (context) => page
-    ),
+    MaterialPageRoute(builder: (context) => page),
   );
 }
 
-closeRoute(BuildContext context){
+closeRoute(BuildContext context) {
   Navigator.pop(context);
 }
 
